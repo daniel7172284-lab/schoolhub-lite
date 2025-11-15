ce617043e9701bff32353c39ecbadb198b985f5d
daniel7172284-lab <daniel7172284@gmail.com>
2025-11-16 01:54:04 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// הגדרות בסיסיות
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+app.use(
+  session({
+    secret: 'schoolhubsecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// דוגמה לעמוד ראשי
+app.get('/', (req, res) => {
+  res.send('SchoolHub Lite פועל בהצלחה 🎓');
+});
+
+// הפעלת השרת
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
